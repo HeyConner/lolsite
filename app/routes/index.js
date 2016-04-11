a0f37f3f65aac031eb5dd743fcd3b31b52ea7206
spawner999 <lore.ferrario@gmail.com>
2016-04-11 13:49:11 -0700

    add api call to retrieve all champs

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  riotApi: Ember.inject.service('riot-api'),
   model(){
-    return this.store.findAll('match');
+    return Ember.RSVP.hash({
+      match: this.store.findRecord('match', 0),
+      champions: this.get('riotApi').getChampions()
+    })
+
   }
 });
--- app/services/riot-api.js
+++ app/services/riot-api.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-  getChampion(champion_id){
-    var url = 'https://na.api.pvp.net/api/lol/{region}/v1.2/champion?api_key=766bed47-44ef-404c-a6a9-12a715c98bc0';
+  getChampions(){
+    var url = 'https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=766bed47-44ef-404c-a6a9-12a715c98bc0&champData=all';
+    return Ember.$.getJSON(url).then(function(response){
+      return response;
+    })
   }
 });
