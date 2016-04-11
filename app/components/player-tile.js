5c61061deae46579a3f6ba8f3ae437ab0b329772
spawner999 <lore.ferrario@gmail.com>
2016-04-11 14:45:05 -0700

    add single champion information

--- app/components/player-tile.js
+++ app/components/player-tile.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  champion: Ember.computed('champions', function(){
+    var championId = this.get('player').championId;
+    var champions = this.get('champions');
+    for(var champion in champions){
+      if(champions[champion].id === championId)
+        return champions[champion];
+    }
+  })
 });
--- app/services/riot-api.js
+++ app/services/riot-api.js
@@ -4,7 +4,7 @@ export default Ember.Service.extend({
   getChampions(){
     var url = 'https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=766bed47-44ef-404c-a6a9-12a715c98bc0&champData=all';
     return Ember.$.getJSON(url).then(function(response){
-      return response;
+      return response.data;
     })
   }
 });
